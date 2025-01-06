53f9fcd1dc7a75d7b223a7db222114bc69109195
MihMihai <bmihai04@gmail.com>
2025-01-06 11:56:53 +0200

    Updated the chopper interceptor to also display errors

--- lib/core/alice_chopper_response_interceptor.dart
+++ lib/core/alice_chopper_response_interceptor.dart
@@ -7,10 +7,13 @@ import 'package:alice/core/alice_utils.dart';
 import 'package:alice/model/alice_form_data_file.dart';
 import 'package:alice/model/alice_from_data_field.dart';
 import 'package:alice/model/alice_http_call.dart';
+import 'package:alice/model/alice_http_error.dart';
 import 'package:alice/model/alice_http_request.dart';
 import 'package:alice/model/alice_http_response.dart';
+import 'package:alice/model/alice_log.dart';
 import 'package:chopper/chopper.dart' as chopper;
 import 'package:chopper/chopper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 
 class AliceChopperInterceptor implements chopper.Interceptor {
@@ -59,69 +62,100 @@ class AliceChopperInterceptor implements chopper.Interceptor {
       ),
     );
 
+    aliceCore.addCall(
+      AliceHttpCall(requestId)
+        ..method = chain.request.method
+        ..endpoint =
+            chain.request.url.path.isEmpty ? '/' : chain.request.url.path
+        ..server = chain.request.url.host
+        ..client = 'Chopper'
+        ..secure = chain.request.url.scheme == 'https'
+        ..uri = chain.request.url.toString()
+        ..request = (AliceHttpRequest()
+          ..size = switch (chain.request.body) {
+            final dynamic body when body is String => utf8.encode(body).length,
+            final dynamic body when body is List<int> => body.length,
+            final dynamic body when body == null => 0,
+            _ => utf8.encode(body.toString()).length,
+          }
+          ..body = chain.request.body ?? ''
+          ..time = DateTime.now()
+          ..headers = chain.request.headers
+          ..contentType =
+              chain.request.headers[HttpHeaders.contentTypeHeader] ?? 'unknown'
+          ..formDataFields = chain.request.parts
+              .whereType<PartValue<String>>()
+              .map(
+                (field) => AliceFormDataField(field.name, field.value),
+              )
+              .toList()
+          ..formDataFiles = chain.request.parts
+              .whereType<PartValueFile<String>>()
+              .map((file) => AliceFormDataFile(file.value, '', 0))
+              .toList()
+          ..queryParameters = chain.request.parameters)
+        ..response = AliceHttpResponse(),
+    );
+
     try {
-      aliceCore.addCall(
-        AliceHttpCall(requestId)
-          ..method = chain.request.method
-          ..endpoint =
-              chain.request.url.path.isEmpty ? '/' : chain.request.url.path
-          ..server = chain.request.url.host
-          ..client = 'Chopper'
-          ..secure = chain.request.url.scheme == 'https'
-          ..uri = chain.request.url.toString()
-          ..request = (AliceHttpRequest()
-            ..size = switch (chain.request.body) {
-              final dynamic body when body is String =>
-                utf8.encode(body).length,
-              final dynamic body when body is List<int> => body.length,
-              final dynamic body when body == null => 0,
-              _ => utf8.encode(body.toString()).length,
-            }
-            ..body = chain.request.body ?? ''
-            ..time = DateTime.now()
-            ..headers = chain.request.headers
-            ..contentType =
-                chain.request.headers[HttpHeaders.contentTypeHeader] ??
-                    'unknown'
-            ..formDataFields = chain.request.parts
-                .whereType<PartValue<String>>()
-                .map(
-                  (field) => AliceFormDataField(field.name, field.value),
-                )
-                .toList()
-            ..formDataFiles = chain.request.parts
-                .whereType<PartValueFile<String>>()
-                .map((file) => AliceFormDataFile(file.value, '', 0))
-                .toList()
-            ..queryParameters = chain.request.parameters)
-          ..response = AliceHttpResponse(),
+      // Make the request to retrieve the response
+      final response = await chain.proceed(request);
+
+      aliceCore.addResponse(
+        AliceHttpResponse()
+          ..status = response.statusCode
+          ..body = response.body ?? ''
+          ..size = switch (response.body) {
+            final dynamic body when body is String => utf8.encode(body).length,
+            final dynamic body when body is List<int> => body.length,
+            final dynamic body when body == null => 0,
+            _ => utf8.encode(body.toString()).length,
+          }
+          ..time = DateTime.now()
+          ..headers = <String, String>{
+            for (final MapEntry<String, String> entry
+                in response.headers.entries)
+              entry.key: entry.value,
+          },
+        requestId,
       );
-    } catch (exception) {
-      AliceUtils.log(exception.toString());
+
+      if (!response.isSuccessful || response.error != null) {
+        aliceCore.addError(
+          AliceHttpError()..error = response.error,
+          requestId,
+        );
+      }
+
+      return response;
+    } catch (error, stackTrace) {
+      /// Log error to Alice log
+      AliceUtils.log(error.toString());
+
+      aliceCore
+        ..addLog(
+          AliceLog(
+            message: error.toString(),
+            level: DiagnosticLevel.error,
+            error: error,
+            stackTrace: stackTrace,
+          ),
+        )
+        /// Add empty response to Alice core
+        ..addResponse(
+          AliceHttpResponse()..status = -1,
+          requestId,
+        )
+        /// Add error to Alice core
+        ..addError(
+          AliceHttpError()
+            ..error = error
+            ..stackTrace = stackTrace,
+          requestId,
+        );
+
+      /// Rethrow error
+      rethrow;
     }
-
-    // Make the request to retrieve the response
-    final response = await chain.proceed(request);
-
-    final httpResponse = AliceHttpResponse()..status = response.statusCode;
-    if (response.body == null) {
-      httpResponse
-        ..body = ''
-        ..size = 0;
-    } else {
-      httpResponse
-        ..body = response.body
-        ..size = utf8.encode(response.body.toString()).length;
-    }
-
-    httpResponse.time = DateTime.now();
-    final headers = <String, String>{};
-    response.headers.forEach((header, values) {
-      headers[header] = values;
-    });
-    httpResponse.headers = headers;
-
-    aliceCore.addResponse(httpResponse, requestId);
-    return response;
   }
 }
