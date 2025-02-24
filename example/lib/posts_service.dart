9b640a18d4b7ad174c547b1c1417dc7091d73010
MihMihai <bmihai04@gmail.com>
2025-02-24 13:34:43 +0200

    Fixed linter issues

--- example/lib/posts_service.dart
+++ example/lib/posts_service.dart
@@ -10,12 +10,12 @@ abstract class PostsService extends ChopperService {
   // helper methods that help you instantiate your service
   static PostsService create([ChopperClient? client]) => _$PostsService(client);
 
-  @Get(path: '/{id}')
+  @GET(path: '/{id}')
   Future<Response<dynamic>> getPost(@Path() String id);
 
-  @Post(path: '/')
+  @POST(path: '/')
   Future<Response<dynamic>> postPost(@Body() String? body);
 
-  @Put(path: '/{id}')
+  @PUT(path: '/{id}')
   Future<Response<dynamic>> putPost(@Path() String id, @Body() String? body);
 }
